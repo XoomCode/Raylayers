aee8f66219929680b85c51e37948d03ddaeaa8bf
Fabio R. Panettieri <fpanettieri@xoomcode.com>
2011-01-10 17:24:34 -0300

    Download and unpack working

--- lib/generators/raylayers/install/install_generator.rb
+++ lib/generators/raylayers/install/install_generator.rb
@@ -1,17 +1,43 @@
-require "raylayers/version"
+require 'zip/zip'
+require 'fileutils'
 
 module Raylayers
   module Generators
     class InstallGenerator < ::Rails::Generators::Base
       desc "This generator downloads and installs OpenLayers"
-      class_option :version, :type => :string, :default => Raylayers::VERSION, :desc => "Which version of OpenLayers to fetch"
-      
+      class_option :version, :type => :string, :default => '2.10', :desc => "Which version of OpenLayers to fetch"
+
       def download_openlayers
+        @tmp_file = "tmp/OpenLayers-#{options.version}.zip"
         say_status("fetching", "OpenLayers (#{options.version})", :green)
-        get "http://openlayers.org/download/OpenLayers-#{options.version}.tar.gz"
+        get "http://openlayers.org/download/OpenLayers-#{options.version}.zip",  @tmp_file
       rescue OpenURI::HTTPError
-        say_status("error", "could not find OpenLayers (#{options.version})", :yellow)
+        say_status("error", "could not find OpenLayers (#{options.version})", :red)
       end
+      
+      def unpack_openlayers
+        say_status("unpacking", @tmp_file, :green)
+        Zip::ZipFile.open(@tmp_file) do |zip|
+          zip.each do |file|
+            file_path = File.join("tmp/", file.to_s)
+            directory = file_path[0, file_path.rindex("/") + 1] 
+            FileUtils.mkdir_p(directory) unless File.directory?(directory)
+            zip.extract(file, file_path) unless File.exists? file_path
+          end
+        end
+      rescue
+        say_status("error", "could not unpack #{@tmp_file}", :red)
+      end
+      
+      def build_openlayers
+        say_status("building", "OpenLayers (#{options.version})", :green)
+        #system("python build.py")
+      end
+      
+      def install_openlayers
+        say_status("installing", "OpenLayers (#{options.version})", :green)
+      end
+      
     end
   end
 end
